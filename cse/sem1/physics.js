1834973ccf0583934736b0a780e612179c7961fb
“Hrithik <ihrithikdev@gmail.com>
2022-04-27 13:35:05 +0530

    physics data added

---
+++ cse/sem1/physics.js
@@ -0,0 +1,47 @@
+const physics = {
+  outline:
+    "Throughout the course, students will be expected to demonstrate their understanding of\
+Discrete Mathematics by being able to do each of the following: ",
+  objectives: [
+    "To impart the basic concepts of data structures and algorithms.",
+    "To understand concepts about searching and sorting techniques",
+    "To understand basic concepts about stacks, queues, lists, trees and graphs.",
+  ],
+  name: "Data Structure & Algorithms",
+  code: "PCC CS 301",
+  credits: 5,
+  content: [
+    {
+      module: "Introduction to DS",
+      topics:
+        "Basic Terminologies: Elementary Data Organizations, Data\
+Structure Operations: insertion, deletion, traversal etc.; Analysis of an\
+Algorithm, Asymptotic Notations, Time-Space trade off.",
+    },
+    {
+      module: "Introduction to DS",
+      topics:
+        "Basic Terminologies: Elementary Data Organizations, Data\
+Structure Operations: insertion, deletion, traversal etc.; Analysis of an\
+Algorithm, Asymptotic Notations, Time-Space trade off.",
+    },
+    {
+      module: "Introduction to DS",
+      topics:
+        "Basic Terminologies: Elementary Data Organizations, Data\
+Structure Operations: insertion, deletion, traversal etc.; Analysis of an\
+Algorithm, Asymptotic Notations, Time-Space trade off.",
+    },
+  ],
+  books: [
+    { name: "Mia", author: "Johny Sins" },
+    { name: "Mia", author: "Johny Sins" },
+    { name: "Mia", author: "Johny Sins" },
+  ],
+
+  outcomes: [
+    "Understand the characteristics of transistors",
+    "Understand the characteristics of transistors",
+    "Understand the characteristics of transistors",
+  ],
+};
